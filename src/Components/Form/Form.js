c8f403435da3b5162be2db28e398b8797ae9be17
conconartist <conconart.info@gmail.com>
2021-03-04 09:18:36 -0700

    Add Form JSX

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -3,11 +3,30 @@ import './Form.css';
 
 const Form = () => {
 
-    return(
-        <form>
-            <p>Synonym</p>
-            <p>Rhyming Words</p>
-        </form>
-    )
+  return(
+    <div className='wordSearchForm'>
+      <form className='similarWordsForm'>
+        <input
+          type='text'
+          placeholder='Similar Words'
+          value={userInput}
+          onChange={searchThesaurus}
+        >
+        </input>
+        <button className='buttonThesaurus' onClick={}></button>
+      </form>
+      <form className='rhymingWordsForm'>
+        <input
+          type='text'
+          placeholder='Rhyming Words'
+          value={userInput}
+          onChange={searchRhymingWords}
+        >
+        </input>
+        <button className='buttonRhymingWords' onClick={}></button>
+      </form>
+    </div>
+    
+  )
 }
 export default Form;
