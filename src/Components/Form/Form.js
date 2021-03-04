1af917bdbab24865e09cfc38aaec9f3179f8699b
conconartist <conconart.info@gmail.com>
2021-03-04 11:19:47 -0700

    Update Form component with useState

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
 
-const Form = () => {
+const Form = ( { displayRhymingWords, displaySimilarWords }) => {
+  const [userInput, setUserInput] = useState('');
+  const searchSimilarWords = (event) => {
+      setUserInput(event.target.value)
+  }
+  const searchRhymingWords = (event) => {
+      setUserInput(event.target.value)
+  }
 
   return(
     <div className='wordSearchForm'>
       <form className='similarWordsForm'>
         <input
           type='text'
-          placeholder='Similar Words'
+          placeholder='Enter Word'
           value={userInput}
-          onChange={searchThesaurus}
+          onChange={searchSimilarWords}
         >
         </input>
-        <button className='buttonThesaurus' onClick={}></button>
+        <button className='buttonThesaurus' onClick={displaySimilarWords}>Get Similar Words</button>
       </form>
       <form className='rhymingWordsForm'>
         <input
           type='text'
-          placeholder='Rhyming Words'
+          placeholder='Enter Word'
           value={userInput}
           onChange={searchRhymingWords}
         >
         </input>
-        <button className='buttonRhymingWords' onClick={}></button>
+        <button className='buttonRhymingWords' onClick={displayRhymingWords}>Get Rhyming Words</button>
       </form>
     </div>
     
