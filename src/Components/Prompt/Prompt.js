cfe22923915e94d07204b0d0a831da9524690b5e
conconartist <conconart.info@gmail.com>
2021-03-04 20:57:22 -0700

    Delete button

--- src/Components/Prompt/Prompt.js
+++ src/Components/Prompt/Prompt.js
@@ -4,10 +4,9 @@ import './Prompt.css';
 const Prompt = ({ clickForPrompt, prompt }) => {
 
     return (
-        <div className='prompt'>
+        <div className='prompt' onClick={clickForPrompt}>
             <h2 className='promptTitle'>Prompt</h2>
             <h3 className='promptSuggestion'>{prompt}</h3>
-            <button className='promptButton' onClick={clickForPrompt}>Click</button>
         </div>
     )
     
