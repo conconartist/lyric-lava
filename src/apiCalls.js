627dd576ef1d9611463eb7892c0ba30dae67c6d4
conconartist <conconart.info@gmail.com>
2021-03-04 20:55:26 -0700

    Update API calls file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,28 @@
+
 const apiCalls = {
-  getRandomWord: () => {
-      return fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "ab8f25f4e4msh6e7ff2ff1b339f9p198212jsn42fc0f56dbc6",
-            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-        }
+  getPrompt: () => {
+    return fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "ab8f25f4e4msh6e7ff2ff1b339f9p198212jsn42fc0f56dbc6",
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+      }
     })
-    .then(response => {
-        console.log(response);
+    .then(response => response.json())
+    .then(data => { 
+      return fetch(`https://wordsapiv1.p.rapidapi.com/words/${data.word}/examples`, {
+	      "method": "GET",
+	      "headers": {
+		    "x-rapidapi-key": "ab8f25f4e4msh6e7ff2ff1b339f9p198212jsn42fc0f56dbc6",
+		    "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+	      }
+      })
     })
-    .catch(err => {
-        console.error(err);
-    });
+    .then(response => response.json())
   }
 }
+
+
+
 
 export default apiCalls;
