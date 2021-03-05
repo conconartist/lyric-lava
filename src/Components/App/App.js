5a56b6370e5bf7280fafd773b5df61988c58b4c0
conconartist <conconart.info@gmail.com>
2021-03-04 20:55:43 -0700

    Fix bug in API calls

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,36 +15,17 @@ class App extends Component {
     }
   }
   clickForPrompt = () => {
-    fetch("https://wordsapiv1.p.rapidapi.com/words/?random=true", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "ab8f25f4e4msh6e7ff2ff1b339f9p198212jsn42fc0f56dbc6",
-        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-      }
-    })
-    .then(response => response.json())
-    .then(data => { 
-    return fetch(`https://wordsapiv1.p.rapidapi.com/words/${data.word}/examples`, {
-	      "method": "GET",
-	      "headers": {
-		    "x-rapidapi-key": "ab8f25f4e4msh6e7ff2ff1b339f9p198212jsn42fc0f56dbc6",
-		    "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-	    }
-    })
-    .then(response => response.json())
-    .then(data => { 
+    apiCalls.getPrompt()
+    .then((data) => { 
       if (data.examples.length === 0) {
-        console.log(data.word)
-          this.setState({ prompt: data.word })
+        this.setState({ prompt: data.word })
       } else {
-        this.state.prompt = data.examples[0]
-      this.setState({ prompt: data.examples[0]})
+        this.setState({ prompt: data.examples[0]})
       }
     })
     .catch(err => {
 	  console.error(err);
-  })
-})
+    });
   }
 
   render() {
