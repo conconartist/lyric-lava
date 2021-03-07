348711f25f44e277fd36687457c0ae2682f9de39
conconartist <conconart.info@gmail.com>
2021-03-06 21:58:48 -0700

    Add Links to WordList

--- src/Components/WordList/WordList.js
+++ src/Components/WordList/WordList.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './WordList.css';
+import { Link } from 'react-router-dom';
 
-const WordList = ({match, word, wordResults, type}) => {
-    console.log(word)
-    console.log(wordResults)
-    console.log(type)
+const WordList = ({word, wordResults, type}) => {
     if (type === 'rhymes') {
         return (
           <div className='resultsContainer'>
             <h3 className='resultsHeading'> Words that rhyme with "{word}": </h3>
             <section className='resultsList'>
-              {wordResults && wordResults.map((word, index) => (
-                <p key={index} className='rhymeWord'>{word}</p>
+              {wordResults && wordResults.map((rhymeWord, index) => (
+                <p key={index} className='rhymeWord'>{rhymeWord}</p>
               ))}
             </section>
+            <Link to='/home'>
+                <button>Home</button>
+            </Link>
           </div>
         )
     } else if(type === 'synonyms') {
@@ -21,10 +22,13 @@ const WordList = ({match, word, wordResults, type}) => {
           <div className="resultsContainer">
             <h3 className="resultsHeading">Words that are similar to "{word}":</h3>
             <section className="resultsList">
-              {wordResults && wordResults.map((word, index) => (
-                <p key={index} className='synonymWord'>{word}</p>
+              {wordResults && wordResults.map((synonymWord, index) => (
+                <p key={index} className='synonymWord'>{synonymWord}</p>
               ))}
             </section>
+            <Link to='/home'>
+                <button>Home</button>
+            </Link>
           </div>
         )
     } else {
