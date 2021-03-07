51866488e8de3d2c8f36c677f89f39f843314f27
conconartist <conconart.info@gmail.com>
2021-03-06 18:03:51 -0700

    Update FormResults conditions

--- src/Components/FormResults/FormResults.js
+++ src/Components/FormResults/FormResults.js
@@ -2,30 +2,42 @@ import React from 'react';
 import './FormResults.css';
 
 const FormResults = ({ word, wordResults, type}) => {
-    if(type === 'rhymes') {
+  if(type === 'rhymes' && wordResults.length <= 10) {
     return (
-      <div className='rhymesContainer'>
-        <h3 className='rhymeHeading'> Words that rhyme with {word} </h3>
-        <section className='resultList'>
+      <div className='resultsContainer'>
+        <h3 className='resultsHeading'> Words that rhyme with {word} </h3>
+        <section className='resultsList'>
           {wordResults && wordResults.map((word, index) => (
-            <p key={index} className='resultWord'>{word}</p>
+            <p key={index} className='rhymeWord'>{word}</p>
           ))}
         </section>
       </div>
+    ) 
+  } else if (type === 'rhymes' && wordResults.length > 10) {
+    return (
+      <h3>Click to see all results</h3>
+        //link to separate page
     )
+    
 //if more than 10 results, display first 10, click option for more on separate page 
 
-    } else if(type === 'synonyms') {
+  } else if(type === 'synonyms' && wordResults.length <= 10) {
     return (
-      <div className="synonymContainer">
-        <h3 className="synonymHeading">Words that are similar to {word}:</h3>
-        <section className="synonymList">
+      <div className="resultsContainer">
+        <h3 className="resultsHeading">Words that are similar to {word}:</h3>
+        <section className="resultsList">
           {wordResults && wordResults.map((word, index) => (
-            <p key={index} className='synonym'>{word}</p>
+            <p key={index} className='synonymWord'>{word}</p>
           ))}
         </section>
       </div>
     )
+  } else if (type === 'rhymes' && wordResults.length > 10) {
+    return (
+        <h3>Click to see all results</h3>
+    )        
+  } else {
+    return null;
   }
 }
 export default FormResults;
