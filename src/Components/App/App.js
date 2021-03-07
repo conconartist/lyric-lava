d2cc07a8b0a4d7822b764af7f0373e105faebf3a
conconartist <conconart.info@gmail.com>
2021-03-06 19:03:16 -0700

    Refactor apiCalls

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -50,14 +50,7 @@ class App extends Component {
 
   searchForRhymes = (searchInput) => {
     if(searchInput) {
-      fetch(`https://wordsapiv1.p.rapidapi.com/words/${searchInput}/rhymes`, {
-	      "method": "GET",
-	      "headers": {
-		      "x-rapidapi-key": "ab8f25f4e4msh6e7ff2ff1b339f9p198212jsn42fc0f56dbc6",
-		      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-	      }
-      })
-      .then(response => response.json())
+      apiCalls.getRhymes(searchInput)
       .then(data => {
         this.setState({ rhymeSearchWord: searchInput, rhymingWords: data.rhymes.all })
       })
