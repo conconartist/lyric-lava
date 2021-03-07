ec0713de72db4f2989d0f00cc2849c6ed1d5f74b
conconartist <conconart.info@gmail.com>
2021-03-06 20:47:45 -0700

    Update Form links

--- src/Components/FormResults/FormResults.js
+++ src/Components/FormResults/FormResults.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import WordList from '../WordList/WordList';
 import './FormResults.css';
+import { Link } from 'react-router-dom';
 
 const FormResults = ({ word, wordResults, type}) => {
   if(type === 'rhymes' && wordResults.length <= 10) {
@@ -22,13 +24,11 @@ const FormResults = ({ word, wordResults, type}) => {
             <p key={index} className='rhymeWord'>{word}</p>
           ))}
         </section>
-        <h4>Click to see all results</h4>
+        <Link to='/results'>
+          <button onClick={() => <WordList key={word} word={word} wordResults={wordResults} type={type}/>}>Click to see all results</button>
+        </Link>
       </div>
-        //link to separate page
     )
-    
-//if more than 10 results, display first 10, click option for more on separate page 
-
   } else if(type === 'synonyms' && wordResults.length <= 10) {
     return (
       <div className="resultsContainer">
@@ -49,7 +49,10 @@ const FormResults = ({ word, wordResults, type}) => {
             <p key={index} className='synonymWord'>{word}</p>
           ))}
         </section>
-        <h4>Click to see all results</h4>
+        <Link to='/results'>
+          <button onClick={() => <WordList key={word} word={word} wordResults={wordResults} type={type}/>}>Click to see all results</button>
+        </Link>
+       
       </div>  
     )        
   } else {
