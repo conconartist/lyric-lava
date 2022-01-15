b9a67cb96be0114b118ae8dcc756a7c9b350df79
conconartist <conconart.info@gmail.com>
2022-01-14 21:30:46 -0700

    Refactor and add component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,6 +7,7 @@ import Prompt from '../Prompt/Prompt';
 import FormResults from '../FormResults/FormResults';
 import Form from '../Form/Form';
 import WordList from '../WordList/WordList';
+import Welcome from '../Welcome/Welcome';
 import './App.css';
 import apiCalls from '../../apiCalls';
 import { Route, Link } from 'react-router-dom';
@@ -92,19 +93,7 @@ const App = () => {
           path='/'
           render={ () => {
             return (
-              <section className="welcomePage">
-                <h2 className='welcomeTitle'>Let your ideas flow.</h2>
-                <div className='welcomeAffirmations'>
-                  <p className='welcome'>Take some time to create.</p>
-                  <p className='welcome'>Remove your distractions.</p>
-                  <p className='welcome'>Get settled in.</p>
-                  <p className='welcome'>Take a deep breath.</p>
-                  <p className='welcome'>Don't judge. Just write.</p>
-                  <Link to='/home'>
-                    <p className='enterButton'>Begin</p>
-                  </Link>
-                </div>
-              </section>
+              <Welcome />
             )
           }}
         />
