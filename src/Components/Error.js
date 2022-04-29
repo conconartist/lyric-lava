2c2b7d352bc61550171945c8658bfa3875eb653d
conconartist <conconart.info@gmail.com>
2022-04-29 13:00:05 -0600

    Fix imports

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/Error.css';
 import PropTypes from 'prop-types';
 
 const Error = ({ type }) => {
--- src/Components/Prompt.js
+++ src/Components/Prompt.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/Prompt.css';
 import PropTypes from 'prop-types';
 
 const Prompt = ({ clickForPrompt, prompt, addToFavorites }) => {
